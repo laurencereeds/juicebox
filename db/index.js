76178ed8e35d0583fde0df499da0a3fd8f73e76c
Laurence Parmentier-Reeds <lauparm@gmail.com>
2020-09-27 20:15:58 -0700

    add connection string

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
 const { Client } = require('pg');
 
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const { DATABASE_URL = 'postgres://localhost:5432/juicebox-dev' } = process.env;
+
+const client = new Client(DATABASE_URL);
 
 async function createUser({ 
   username, 
